adee52350653c4b34fb0c102b2bbf3e16afc41b3
David Beath <davidgbeath@gmail.com>
2014-08-31 12:47:36 +1200

    Updated registration model

--- models/registrations.js
+++ models/registrations.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 var regSchema = mongoose.Schema({
   email: { type: String, required: true, index: { unique: true } },
+  firstname: { type: String },
+  lastname: { type: String },
   registered: { type: Date, default: Date.now }
 });
 
